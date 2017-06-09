5cddb4de7dfa31e4465f7abefd2f9ff1c37610ac
dhruv chauhan <dc14690@my.bristol.ac.uk>
2017-06-09 16:08:19 +0200

    added new arduino code

--- software/arduinopressuresensor/arduinopressuresensor.ino
+++ software/arduinopressuresensor/arduinopressuresensor.ino
@@ -1,19 +1,21 @@
 //Filtered Analog Input
-//This Sketch is designed for playing with Pencil-drawn voltage dividers
-//it uses an aggressive low-pass filter to stabalize the readings.
-//Written by Paul Strohmeier (paul.strohmeier@gmail.com) 
-//for the User Interface Technologies class at University of Copenhagen
-
-float incomingValue;    // stores the value red from analog input
-float incomingValue2;
-float cleanValue;       // stores the filtered value
-float cleanValue2;       // stores the filtered value
-float oldValue = 512;   // stores the previous clean value
-float oldValue2 = 512;   // stores the previous clean value
+//group 1 - user interface technology unit
+//main code for deformaball
+float incomingValue0;    // stores the value red from analog input
+float incomingValue1;
+float cleanValue0;       // stores the filtered value
+float cleanValue1;       // stores the filtered value
+float oldValue0 = 512;   // stores the previous clean value
+float oldValue1 = 512;   // stores the previous clean value
+float value0;
+float value1;
+int mapped_val_0;
 int mapped_val_1;
-int mapped_val_2;
+float calibrate0;
+float calibrate1;
+int calibrateCounter = 0;
+int calibrateIterations = 1000;
 float k = 0.1;          // sets the ration of new-to-old signal. 1 == no filter 0 == no signal
-
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -21,44 +23,53 @@ void setup() {
  pinMode(A0, INPUT_PULLUP);
   pinMode(A1, INPUT_PULLUP);
 }
-
 // the loop routine runs over and over again forever:
 void loop() {
 //we don't really need the following print statements, 
 //but they make the visualization of the serial plotter nicer
-  //Serial.print(0); 
-  //Serial.print(", ");
-  //Serial.print(1025);
-  //Serial.print(", ");
   
-  incomingValue = analogRead(A0);       // read the input on analog pin 0:
-  incomingValue2 = analogRead(A1); // read the input on analog pin 1
-  cleanValue = oldValue + (incomingValue - oldValue) * k; //filter i  t
-  cleanValue2 = oldValue2 + (incomingValue2 - oldValue2) * k; //filter it
-  oldValue = cleanValue;
-  oldValue2 = cleanValue2; // store value for later use
+ incomingValue0 = analogRead(A0);       // read the input on analog pin 0:
+  incomingValue1 = analogRead(A1); // read the input on analog pin 1
+  cleanValue0 = oldValue0 + (incomingValue0 - oldValue0) * k; //filter i  t
+  cleanValue1 = oldValue1 + (incomingValue1 - oldValue1) * k; //filter it
+  oldValue0 = cleanValue0;
+  oldValue1 = cleanValue1; // store value for later use
 
-  mapped_val_1 = map(cleanValue, 250, 30, 0, 100);
-  mapped_val_2 = map(cleanValue2, 250, 30, 0, 100); 
+ if(calibrateCounter < calibrateIterations){
 
-//  if(cleanValue2 > 390 && cleanValue2 < 440){
-//    Serial.print(0);
-//  }else{
-//    Serial.print(mapped_val_1);
-    Serial.print(cleanValue);
-//  }
+   calibrate0 += cleanValue0;
+    calibrate1 += cleanValue1;
+    calibrateCounter++;
+    Serial.println(calibrateCounter);
+    return;
+  }
+  else if(calibrateCounter == calibrateIterations){
+
+   calibrate0 = calibrate0 / calibrateIterations;
+    calibrate1 = calibrate1 / calibrateIterations;
+    Serial.println(calibrate0);
+    calibrateCounter++;
+    return;
+  }
+
+ value0 = calibrate0 - cleanValue0;
+ value1 = calibrate1 - cleanValue1;
+
+ if(value0 < 15){
+    value0 = 0;
+  }
+  if(value1 < 15){
+    value1 = 0;
+  }
+
+// mapped_val_0 = map(value0, 250, 30, 0, 100);
+//  mapped_val_1 = map(value1, 250, 30, 0, 100);
   
-  Serial.print(" ");
-//  if(cleanValue > 289 && cleanValue < 350){
-//    Serial.println(0);
-//  }else{
-//  Serial.println(mapped_val_2);
-  Serial.println(cleanValue2);
-//  Serial.print(" ");
-//  Serial.println(cleanValue2);  // print the filtered value
-
-//  }
+ Serial.print(value0);
+ Serial.print(" ");
+ Serial.println(value1);
   
-  delay(1);        // slow down the speed at which its dumped to serial a bit...
+ 
+ delay(1);        // slow down the speed at which its dumped to serial a bit...
                    // (beware: delay command can break timing-critical code!)
 }
